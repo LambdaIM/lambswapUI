0ab305c31efedf8ec994ba24f2299a3fb223e324
Kerinlin <18296613547@qq.com>
2021-04-02 20:14:22 +0800

    Add claim methods

---
+++ src/components/extractDialog.vue
@@ -0,0 +1,221 @@
+<template>
+  <div class="claim-dialog">
+    <Modal v-model="openClaimDialog" class-name="claim-modal" :footer-hide="true" :closable="true">
+      <div class="claim-content">
+        <p class="title text-center">
+          {{ $t('myPage.claim') }}
+        </p>
+        <div class="claim-wrapper">
+          <div class="title-content">
+            <span class="card-title">{{ $t('common.amount') }}</span>
+            <div class="balance-item">
+              <span class="mr-2 text-secondary">{{ $t('myPage.dialog.claim.unclaim') }}</span>
+              <span>{{ claimAmount }} LAMB</span>
+            </div>
+          </div>
+          <div class="claim-wrapper">
+            <input v-model="claimAmount" readonly type="text" class="amount-input">
+          </div>
+        </div>
+
+        <div class="btn-warpper">
+          <Buttons v-if="!extractAirdropLoading" @click.native="sendExtractAirdrop">
+            {{ $t('common.confirm') }}
+          </Buttons>
+          <Buttons v-else>
+            {{ $t('common.loading') }}...
+          </Buttons>
+        </div>
+      </div>
+    </Modal>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import { fetchRewardAddress } from '@/contactLogic/earn/Reward.js';
+import event from '@/common/js/event';
+const BigNumber = require('bignumber.js');
+BigNumber.config({ DECIMAL_PLACES: 6, ROUNDING_MODE: BigNumber.ROUND_DOWN });
+import { withdrawAirDropValue } from '@/contactLogic/AirDrop.js';
+export default {
+  data() {
+    return {
+      openClaimDialog: false,
+      claimAmount: '',
+      data: {},
+      coinName: '',
+      extractAirdropLoading: false,
+      fee: '',
+      rewardToken: '',
+    };
+  },
+  methods: {
+    open(data) {
+      console.log({ data });
+      this.claimAmount = data;
+      this.openClaimDialog = true;
+    },
+    checkData() {
+      if (this.claimAmount <= 0) {
+        this.$Notice.warning({
+          title: this.$t('notice.n'),
+          desc: this.$t('notice.n31'),
+        });
+        return false;
+      }
+      return true;
+    },
+    async getAddress() {
+      const obj = {};
+      try {
+        const web3 = this.web3;
+        const chainID = this.ethChainID;
+        const account = this.ethAddress;
+        const library = this.ethersprovider;
+        const tokenName = 'LAMB';
+        const address = await fetchRewardAddress({ web3, chainID, account, library, tokenName });
+        obj.address = address;
+        return obj;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+    // 提取收益
+    async sendExtractAirdrop() {
+      if (!this.checkData()) {
+        return false;
+      }
+      this.extractAirdropLoading = true;
+      try {
+        const chainID = this.ethChainID;
+        const account = this.ethAddress;
+        const library = this.ethersprovider;
+        const result = await withdrawAirDropValue(library, account, chainID);
+        // console.log('领取的奖励', result);
+        this.$Notice.success({
+          title: this.$t('notice.n33'),
+        });
+        event.$emit('sendtx', [
+          result,
+          {
+            okinfo: `${this.$t('common.claimAirdrop')} ${this.claimAmount} LAMB ${this.$t('notice.n42')}`,
+            failinfo: `${this.$t('common.claimAirdrop')} ${this.$t('notice.n43')}`,
+          },
+        ]);
+      } catch (error) {
+        console.log(error);
+        this.$Notice.error({
+          title: this.$t('notice.n32'),
+        });
+      } finally {
+        const timer = setTimeout(() => {
+          this.openClaimDialog = false;
+          this.extractAirdropLoading = false;
+          clearTimeout(timer);
+        }, 1000);
+      }
+    },
+  },
+  components: {
+    Buttons: () => import('@/components/basic/buttons'),
+  },
+  computed: {
+    ...mapState(['ethersprovider', 'ethChainID', 'ethAddress', 'chainTokenPrice', 'web3']),
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.claim-modal {
+  width: 500px;
+  background: #ffffff;
+  box-shadow: 0px 0px 40px 0px rgba(0, 0, 0, 0.06);
+  border-radius: 12px;
+  .claim-content {
+    padding: 14px 28px;
+    .title {
+      height: 28px;
+      font-size: 24px;
+      font-weight: 500;
+      color: #14171c;
+      line-height: 28px;
+      margin-bottom: 24px;
+    }
+    .claim-wrapper {
+      .title-content {
+        overflow: hidden;
+        margin: 24px 0 8px;
+        span {
+          float: left;
+        }
+        .balance-item {
+          float: right;
+          font-size: 12px;
+          font-weight: 500;
+          line-height: 14px;
+        }
+      }
+      .claim-wrapper {
+        width: 100%;
+        height: 72px;
+        background: #f7f8f9;
+        border-radius: 6px;
+        position: relative;
+        .amount-input {
+          width: 100%;
+          height: 100%;
+          outline: none;
+          border: none;
+          background: #f7f8f9;
+          font-size: 40px;
+          line-height: 47px;
+          color: #14171c;
+          padding: 16px;
+          caret-color: #0058ff;
+          &:focus {
+            border: 1px solid #0058ff;
+            border-radius: 4px;
+          }
+        }
+        .unit {
+          cursor: pointer;
+          position: absolute;
+          right: 16px;
+          top: 28px;
+        }
+      }
+    }
+    .price-warpper {
+      margin-top: 30px;
+      .price-item {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      }
+      span {
+        height: 14px;
+        font-size: 12px;
+        font-weight: 500;
+        color: #828489;
+        line-height: 14px;
+      }
+      p {
+        height: 14px;
+        font-size: 12px;
+        font-weight: 500;
+        color: #14171c;
+        line-height: 14px;
+        margin-bottom: 8px;
+      }
+    }
+    .btn-warpper {
+      margin-top: 24px;
+      .button-wrapper {
+        height: 48px;
+      }
+    }
+  }
+}
+</style>
--- src/contactLogic/AirDrop.js
+++ src/contactLogic/AirDrop.js
@@ -4,55 +4,55 @@ import _ from 'underscore';
 // import { AirDropper_address } from '@/constants/index.js'
 
 import {
-    calculateGasMargin,
-    getRouterContract,
-    calculateSlippageAmount,
+  calculateGasMargin,
+  getRouterContract,
+  calculateSlippageAmount,
 } from "@/contacthelp/utils.js";
 
 
 export async function userAirDropValue(
-    library,
-    account,
-    chainId
+  library,
+  account,
+  chainId
 ) {
-    
-   const tokenConig= _.find(AirDropconfig,{name:'AirDrop',chainId:chainId});
-    const contract = useAirDropContract(library, account, tokenConig.address, false);
+
+  const tokenConig = _.find(AirDropconfig, { name: 'AirDrop', chainId: chainId });
+  const contract = useAirDropContract(library, account, tokenConig.address, false);
 
 
-    let rewarder = 0;
-    try {
-        rewarder= await contract.rewarders(account);
-    } catch (error) {
-        console.log(error);
-    }
-    return rewarder;
+  let rewarder = 0;
+  try {
+    rewarder = await contract.rewarders(account);
+  } catch (error) {
+    console.log(error);
+  }
+  return rewarder;
 
 }
 
 
 export async function withdrawAirDropGas(library,
-    account,chainId) {
-        console.log('--');
-  const tokenConig= _.find(AirDropconfig,{name:'AirDrop',chainId:chainId});
-    const contract = useAirDropContract(library, account, tokenConig.address, false);
-    const dataGas = contract.estimateGas.withdraw();
+  account, chainId) {
+  console.log('--');
+  const tokenConig = _.find(AirDropconfig, { name: 'AirDrop', chainId: chainId });
+  const contract = useAirDropContract(library, account, tokenConig.address, true);
+  const dataGas = contract.estimateGas.withdraw();
 
-    return dataGas;
+  return dataGas;
 
 }
 
 export async function withdrawAirDropValue(library,
-    account,chainId) {
+  account, chainId) {
 
-    const tokenConig= _.find(AirDropconfig,{name:'AirDrop',chainId:chainId});
-    const estimatedGasLimit = await withdrawAirDropGas(library,
-        account,chainId);
-    const contract = useAirDropContract(library, account, tokenConig.address, true);
-    const dataGas = contract.withdraw({
-        gasLimit: calculateGasMargin(estimatedGasLimit),
-    });
+  const tokenConig = _.find(AirDropconfig, { name: 'AirDrop', chainId: chainId });
+  const estimatedGasLimit = await withdrawAirDropGas(library,
+    account, chainId);
+  const contract = useAirDropContract(library, account, tokenConig.address, true);
+  const dataGas = contract.withdraw({
+    gasLimit: calculateGasMargin(estimatedGasLimit),
+  });
 
-    return dataGas;
+  return dataGas;
 
 }
