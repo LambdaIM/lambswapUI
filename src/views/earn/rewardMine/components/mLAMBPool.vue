ae01891d515cab41089b4905cb6a2de16254e987
r-z-x <844818249@qq.com>
2021-04-27 16:16:41 +0800

    earn页面新加mLAMB质押的移动端

--- src/views/earn/rewardMine/components/mLAMBPool.vue
+++ src/views/earn/rewardMine/components/mLAMBPool.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mlambPool-wrapper mb-6">
+  <div v-if="!isMobile" class="mlambPool-wrapper mb-6">
     <div class="title-wrapper flex justify-between items-center">
       <div class="left">
         <span class="title">mLAMB</span>
@@ -49,26 +49,77 @@
 
     <pledgeDialog ref="pledge" />
   </div>
+
+  <div v-else class="mlambPool-wrapper-mobile mb-6">
+    <div class="title-wrapper flex justify-between items-center">
+      <div class="left">
+        <span class="title">mLAMB</span>
+        <!-- <span class="next">{{ $t('liquidity.mlambPool.next') }}</span> -->
+      </div>
+      <div class="right">
+        mLAMB质押
+      </div>
+    </div>
+    <div class="mlambPool-Mobile">
+      <div class="mlambPool-item name">
+        <img src="../../../../assets/img/mlamb48.svg" alt="mlambda">
+        <span>mLAMB bar</span>
+      </div>
+
+      <div class="mlambPool-item volume flex items-center justify-between">
+        <p class="drop-title">
+          APY
+        </p>
+        <p class="drop-num">
+          100%
+        </p>
+
+        <!-- <p class="tag">
+          <span class="mine font10px">{{ $t('liquidity.mlambPool.tag') }}</span>
+        </p> -->
+      </div>
+
+      <div class="mlambPool-item countdown flex items-center justify-between">
+        <p class="drop-title">
+          总资产
+        </p>
+        <p class="mt-1">
+          {{ listData && listData.data && listData.data.totalAsset }} XmLAMB
+        </p>
+      </div>
+
+      <div class="mlambPool-item create">
+        <button v-if="ethAddress" class="createBtn" @click="openDialog">
+          存入资产
+        </button>
+        <button v-else class="createBtn disableBtn">
+          存入资产
+        </button>
+      </div>
+    </div>
+
+    <pledgeDialog ref="pledge" />
+  </div>
 </template>
 
 <script>
 // import event from '@/common/js/event';
-import { getFarmList } from '../../utils/helpUtils/mineUtilFunc.js';
-import { mapState } from 'vuex';
-const BigNumber = require('bignumber.js');
+import { getFarmList } from "../../utils/helpUtils/mineUtilFunc.js";
+import { mapState } from "vuex";
+const BigNumber = require("bignumber.js");
 BigNumber.config({ DECIMAL_PLACES: 6, ROUNDING_MODE: BigNumber.ROUND_DOWN });
 export default {
   data() {
     return {
-      listData: {}
+      listData: {},
     };
   },
   components: {
-    countDown: () => import('@/components/basic/countDown.vue'),
-    pledgeDialog: () => import('../dialog/mlambPledge/mlambPledge.vue'),
+    countDown: () => import("@/components/basic/countDown.vue"),
+    pledgeDialog: () => import("../dialog/mlambPledge/mlambPledge.vue"),
   },
   computed: {
-    ...mapState(['ethChainID', 'ethAddress', 'ethersprovider']),
+    ...mapState(["ethChainID", "ethAddress", "ethersprovider", "isMobile"]),
     isReady() {
       return this.ethChainID && this.ethersprovider && this.ethAddress;
     },
@@ -84,7 +135,7 @@ export default {
         const library = this.ethersprovider;
         const [result] = await getFarmList(library, account, chainID);
         this.listData = result;
-        console.log({result});
+        console.log({ result });
       } catch (error) {
         console.log(error);
       }
@@ -191,6 +242,36 @@ export default {
       .count {
         margin-top: 14px;
       }
+    }
+  }
+}
+.mlambPool-wrapper-mobile {
+  width: 100%;
+  background: #ffffff;
+  box-shadow: 0px 0px 40px 0px rgba(0, 0, 0, 0.06);
+  border-radius: 12px;
+  padding: 20px 16px;
+  .title-wrapper {
+    margin-bottom: 24px;
+  }
+  .mlambPool-Mobile {
+    background: #ffffff;
+    box-shadow: 0px 0px 40px 0px rgba(0, 0, 0, 0.06);
+    border-radius: 12px;
+    padding: 20px 16px;
+    margin-bottom: 16px;
+    .name {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      img {
+        max-width: 48px;
+        margin-bottom: 8px;
+      }
+    }
+    .createBtn{
+      width: 100%;
+      margin-top: 16px;
     }
   }
 }
